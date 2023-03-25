783fb8a73f95e5fae90b39dbe9cda43f61f6d35e
ilonashkil <ilonashkil@gmail.com>
2023-03-25 12:45:06 +0300

    Update Spotify_Streams_Project.sql

--- Spotify_Data_Analysis/Spotify_Streams_Project.sql
+++ Spotify_Data_Analysis/Spotify_Streams_Project.sql
@@ -99,4 +99,33 @@ ORDER BY s.streams_billions DESC;
 
 
 
+-- Basic stats: 
+-- 1) Which song is longest and which is the shortest?
+SELECT s.artist, f.name, f.duration
+FROM spot_features f
+         JOIN spot_streams s
+              ON s.song = f.name
+WHERE f.duration =
+      (SELECT MAX(duration) FROM spot_features)
+   OR f.duration =
+      (SELECT MIN(duration) FROM spot_features);
+
+-- 2) What song with a duration that equals to the average?
+SELECT s.artist, f.name, f.duration
+FROM spot_features f
+         JOIN spot_streams s
+              ON s.song = f.name
+WHERE f.duration = (SELECT TRUNC(AVG(duration), 2) FROM spot_features);
+
+-- 3) How many songs are longer than the average and how many are shorter?
+SELECT COUNT(*) AS count,
+       CASE
+           WHEN duration < (SELECT TRUNC(AVG(duration), 2) FROM spot_features) THEN 'Below average'
+           WHEN duration > (SELECT TRUNC(AVG(duration), 2) FROM spot_features) THEN 'Above average'
+           ELSE 'Average'
+           END  AS duration_groups
+FROM spot_features
+GROUP BY duration_groups;
+    
+
 --- TO BE CONTINUED --- 
